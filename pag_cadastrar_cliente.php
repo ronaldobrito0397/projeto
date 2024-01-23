fa203707537a4bc2e4332cdb1fb10e94f1d54f76
Ronaldo <rndebno@gmail.com>
2024-01-23 07:57:31 -0300

    Ajuste de nomeclatura de campos Input

--- pag_cadastrar_cliente.php
+++ pag_cadastrar_cliente.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Paciente</title>
+    <title>Cadastro de Pacientes</title>
 </head>
 <body>
     <style>
@@ -57,7 +57,7 @@
     </style>
     <center>
     <header>
-        <h1>Cadastro de Paciente</h1>
+        <h1>Cadastro de Pacientes</h1>
     </header>
         <form action="verificarCadastro.php" method="post">
             Nome Completo : <input type="text" name="txtnome" placeholder="Digite o Nome do Paciente"><br>
